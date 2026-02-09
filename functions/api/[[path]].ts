32c27469ce83cab35b8343f317fc7ae934038ee8
JINSU PARK <wlstntrtr@gmail.com>
2026-02-10 05:09:50 +0900

    deploy: trigger new build with version bump

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -1,3 +1,4 @@
+// Deployment Version: 2026-02-10T05:10
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "../../server/routers";
 import { getDb } from "../../server/db";
